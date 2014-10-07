97234b4fb0f85f6f2b1113b3e72d9e1e2a9b49b8
Ryan Jurgensen <ryan.jurgensen@gmail.com>
2014-10-07 10:32:22 -0700

    cleanup

--- routes/index.js
+++ routes/index.js
@@ -3,28 +3,26 @@ var express = require('express');
 var router = express.Router();
 var mg = require('mailgun');
 
-/* GET home page. */
 router.get('/', function(req, res) {
-	res.render('index', { 
-		title: 'Express', 
-		csrfToken: req.csrfToken() 
-	});
+  res.render('index', { 
+    title: 'Ryan Jurgensen - Freelance Web Developer', 
+    csrfToken: req.csrfToken() 
+  });
 });
 
 router.post('/contact', function(req, res){
-	console.log(req.body)
-	var name = req.body.name,
-	    email = req.body.email,
-	    message = req.body.message
-			mailgun = new mg.Mailgun(process.env.MAILGUNKEY);
+  var name = req.body.name,
+      email = req.body.email,
+      message = req.body.message
+    mailgun = new mg.Mailgun(process.env.MAILGUNKEY);
 
   mailgun.sendText('ryan@ryanjurgensen.com', 
-  	['ryan@ryanjurgensen.com'], 
-  	'New lead from website', 
-  	'Name: ' + name + '\nEmail: ' + email + '\nMessage: ' + message,
-  	function(err) {
-	  	if(err) res.send(500)
-	  	res.send(200)
+    ['ryan@ryanjurgensen.com'], 
+    'New lead from website', 
+    'Name: ' + name + '\nEmail: ' + email + '\nMessage: ' + message,
+    function(err) {
+      if(err) res.send(500)
+      res.send(200)
   });
 });
 module.exports = router;
